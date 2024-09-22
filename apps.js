344afeb1bd75643c72e061cf8967d33c4d54df3a
Baloyi2 <baloyinwayitelo6@gmail.com>
2024-09-23 00:59:33 +0200

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,14 @@
+function goToMathQuiz() {
+    // Redirect to Math Quiz page
+    window.location.href = 'math_quiz.html'; // Change this to the actual URL
+}
+
+function goToMathQuestions() {
+    // Redirect to Math Questions and Answers page
+    window.location.href = 'math_questions.html'; // Change this to the actual URL
+}
+
+function goToUserHome() {
+    // Redirect to User Home page
+    window.location.href = 'user_home.html'; // Change this to the actual URL
+}
