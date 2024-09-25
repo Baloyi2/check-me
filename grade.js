eb42b4401bfcb308a1eda350c6ba2aeda197b0b7
Baloyi2 <baloyinwayitelo6@gmail.com>
2024-09-25 13:49:51 +0200

    Add files via upload

--- grade.js
+++ grade.js
@@ -1,3 +1,8 @@
 function goToGrade(grade) {
     window.location.href = `grade${grade}.html`; // Adjust URLs as needed
 }
+// In grade.js
+function goToApps() {
+    window.location.href = 'apps.html'; // Redirect to the apps page
+}
+
